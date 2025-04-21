8a9a0cf2f25e9b3d5f723981fc491c5e4444f13f
cashew <cashewstyle241224@yahoo.co.jp>
2025-04-21 09:58:47 +0900

    todolist57

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [inputText, setInputText] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "TODOです1",
+    "TODOです2",
+  ]);
+  const [completeTodos, setCompleteTodos] = useState([
+    "TODOでした1",
+    "TODOでした2",
+  ]);
+
+  const onChangeText = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const onClickAdd = () => {
+    if (inputText === "") return;
+    const newTodos = [...incompleteTodos, inputText];
+    setIncompleteTodos(newTodos);
+    setInputText("");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="input-area">
+        <input
+          type="text"
+          placeholder="TODOを入力"
+          onChange={onChangeText}
+          value={inputText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todo) => (
+            <li key={todo}>
+              <div className="list-row">
+                <p className="list-item">{todo}</p>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+      <div className="complete-area">
+        <p className="title">完了したTODO</p>
+        <ul>
+          {completeTodos.map((todo) => (
+            <li key={todo}>
+              <div className="list-row">
+                <p className="list-item">{todo}</p>
+                <button>戻す</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
